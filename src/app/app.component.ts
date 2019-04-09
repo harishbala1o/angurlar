0018765fb44b4ee4b6f062e4af86dda020a4b21c
harishbala1o <36741511+harishbala1o@users.noreply.github.com>
2019-04-09 16:28:42 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'my first app';
+  ngOnInit() {
+    (function(d, m){
+      var kommunicateSettings = {"appId":"kommunicate-support","conversationTitle":"Devashish"};
+      var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+      s.src = "https://api.kommunicate.io/v2/kommunicate.app";
+      var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+      (window as any).kommunicate = m; m._globals = kommunicateSettings;
+    })(document, (window as any).kommunicate || {});
+  }
+}
